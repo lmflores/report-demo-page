0c03700b1bf62e2c0bdaa6787ce127846fba80a1
Luis Flores <98134740+lmflores@users.noreply.github.com>
2022-09-29 09:46:55 -0500

    Actualización hasta el carrito

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 #from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
-#from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome(executable_path=r"C:\Users\luism\OneDrive\Documentos\AA Trabajos 7to Parcial 22_2\Prueba Aseg Calidad Soft\ChromeDriver\chromedriver.exe")
@@ -10,18 +9,20 @@ driver.get("https://www.demoblaze.com/index.html")
 driver.maximize_window()
 alert = Alert(driver)
 
+def dataUser(username_id, password_id):
+    user = driver.find_element(By.ID, username_id)
+    user.send_keys("lmflores")
+    password = driver.find_element(By.ID, password_id)
+    password.send_keys("paralelepipedo80")
+
 #----------------Registro---------------#
 
 driver.find_element(By.ID, 'signin2').click()
 time.sleep(2)
 
-user = driver.find_element(By.ID, "sign-username")
-user.send_keys("lmflores")
-password = driver.find_element(By.ID, "sign-password")
-password.send_keys("paralelepipedo80")
-time.sleep(2)
+dataUser("sign-username", "sign-password")
 driver.find_element(By.XPATH, '//*[@id="signInModal"]/div/div/div[3]/button[2]').click()
-time.sleep(2)
+time.sleep(5)
 alert.accept()
 driver.find_element(By.XPATH, '//*[@id="signInModal"]/div/div/div[1]/button').click()
 
@@ -30,20 +31,31 @@ driver.find_element(By.XPATH, '//*[@id="signInModal"]/div/div/div[1]/button').cl
 driver.find_element(By.ID, 'login2').click()
 time.sleep(2)
 
-user = driver.find_element(By.ID, "loginusername")
-user.send_keys("lmflores")
-password = driver.find_element(By.ID, "loginpassword")
-password.send_keys("paralelepipedo80")
-time.sleep(2)
+dataUser("loginusername", "loginpassword")
 driver.find_element(By.XPATH, '//*[@id="logInModal"]/div/div/div[3]/button[2]').click()
-time.sleep(2)
-#driver.find_element(By.XPATH, '//*[@id="logInModal"]/div/div/div[1]/button').click()
 
 #----------Agregar al carrito-----------#
 
-articles = {'Phones': ['//*[@id="tbodyid"]/div[2]/div/div/h4/a', '//*[@id="tbodyid"]/div[5]/div/div/h4/a'],
-            'Laptops': ['//*[@id="tbodyid"]/div[2]/div/div/h4/a', '//*[@id="tbodyid"]/div[3]/div/div/h4/a'],
-            'Monitors': ['//*[@id="tbodyid"]/div[1]/div/div/h4/a', '//*[@id="tbodyid"]/div[2]/div/div/h4/a']}
+articles = {
+            "Phones" : ['//*[@id="tbodyid"]/div[2]/div/div/h4/a', '//*[@id="tbodyid"]/div[5]/div/div/h4/a'],
+            "Laptops" : ['//*[@id="tbodyid"]/div[2]/div/div/h4/a', '//*[@id="tbodyid"]/div[3]/div/div/h4/a'],
+            "Monitors" : ['//*[@id="tbodyid"]/div[1]/div/div/h4/a', '//*[@id="tbodyid"]/div[2]/div/div/h4/a']
+            }
 
-driver.find_element(By.LINK_TEXT, ).click()
-driver.find_element(By.XPATH, ).click()
+for categories, products in articles.items():
+    for product in products:
+        time.sleep(2)
+        driver.find_element(By.LINK_TEXT, categories).click()
+        time.sleep(2)
+        driver.find_element(By.XPATH, product).click()
+        time.sleep(1)
+        driver.find_element(By.LINK_TEXT, "Add to cart").click()
+        time.sleep(2)
+        alert.accept()
+        driver.find_element(By.XPATH, '//*[@id="navbarExample"]/ul/li[1]/a').click()
+        
+
+#-------------Ir al carrito-------------#
+
+time.sleep(2)
+driver.find_element(By.ID, 'cartur').click()
